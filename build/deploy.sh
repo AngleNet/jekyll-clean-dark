8bbca63ecae3feaefd4c3247263bdc782a3ab4b5
AngleNet <wangmin1123@iie.ac.cn>
2016-08-11 16:09:37 +0800

    Commit new blog

---
+++ build/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#jekyll build --incremental
+#cp -rf ./_site/* ../AngleNet.github.io/
+#cd ../AngleNet.github.io
+git add -A
+git commit -m "Commit new blog"
+git push
+
+rm -rf _site
+jekyll build
+cd ../AngleNet.github.io
+git rm -rf *
+mv ../blog/_site/* .
+git add -A
+git commit -m "Commit a new blog"
+git push
