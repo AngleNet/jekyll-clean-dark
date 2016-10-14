2dbb3d67cd546eb370bcbb8be9844e69088d270f
AngleNet <anglenet@sina.cn>
2016-10-14 22:04:48 +0800

    Commit new blog

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,15 @@
 #cd ../AngleNet.github.io
 blog=/home/anglenet/repo/anglenet.github.io
 local_url="http://localhost:7777"
-online_url="http://anglenet.github.io"
+online_url="https://anglenet.github.io"
 
 sed -i /^url/d _config.yml
 echo "url: $online_url" >> _config.yml
-sed -i /^url/d _config.yml
-echo "url: $local_url" >> _config.yml
 rm -rf _site
 jekyll build
+sed -i /^url/d _config.yml
+echo "url: $local_url" >> _config.yml
+
 cd $blog && git rm -rf *
 cd -
 mv _site/* $blog
