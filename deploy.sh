ebc1762dcc1e167a984b338c0b956527ba3097d8
AngleNet <anglenet@sina.cn>
2016-10-14 11:57:51 +0800

    Commit new blog

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 #jekyll build --incremental
 #cp -rf ./_site/* ../AngleNet.github.io/
 #cd ../AngleNet.github.io
+blog=/home/anglenet/repo/anglenet.github.io
 git add -A
 git commit -m "Commit new blog"
 git push
 
 rm -rf _site
 jekyll build
-cd ../anglenet.github.io
-git rm -rf *
-mv ../blog/_site/* .
-git add -A
-git commit -m "Commit a new blog"
-git push
+cd $blog && git rm -rf *
+cd -
+mv _site/* $blog
+
+cd $blog && git add -A && git commit -m "Commit a new blog" && git push
